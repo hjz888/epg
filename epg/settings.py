6f188d716f07498136ef94fb17e42f3e2ae7fb78
hjz888 <2702794@163.com>
2023-07-18 16:15:26 +0800

    Update settings.py

--- epg/settings.py
+++ epg/settings.py
@@ -120,3 +120,4 @@ STATICFILES_DIRS =[os.path.join(BASE_DIR,'static')]
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CSRF_TRUSTED_ORIGINS = ['http://10.10.10.14/epg']
